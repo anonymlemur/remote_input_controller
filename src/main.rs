f168d3ca906986428ace1cbb2379d6c7b5949aa6
Joel <j.jagestedt@gmail.com>
2024-04-14 22:01:04 +0200

    modified:   .vscode/launch.json 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-
 pub mod input;
 pub mod web_socket;
 
+use std::process;
 
 #[tokio::main]
 async fn main() {
     let address = "192.168.1.46:9000";
-    web_socket::run_server(address).await;
-}
 
+    // Attempt to run the web socket server and handle potential errors
+    if let Err(e) = web_socket::run_server(address).await {
+        eprintln!("Failed to start the server: {:?}", e);
+        //restart process
+        process::exit(1);
+
+    }
+}
